e449178ab791d35ee1ce94e5f037053d3415c042
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-12 22:58:18 +0400

    Create Folder And Files and Setup The Configuration

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,2 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,21 @@ const app = express();
 
 app.use(express.json());
 
+// ?? Routes
+app.use("/api/v1/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDB();
+});
+
+// *middleware for handle errors:
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
