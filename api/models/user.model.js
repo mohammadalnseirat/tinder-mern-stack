6e8f3bf650922521a27d939fac2c2052493265fd
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-12 23:06:49 +0400

    Create User Model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,9 +1,64 @@
 import mongoose from "mongoose";
 
-
-const userSchema = new mongoose.Schema({},{timestamps: true});
-
-
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    genderPreference: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "both"],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    matches: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
-export default User
+export default User;
