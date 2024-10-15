6425874faf5e844270e34092bb65e07dac47364b
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-16 02:05:45 +0400

    Create Sign Up Api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,64 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// ?Function to create Token:
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "7d",
+  });
+};
 //! 1-Function to signup user:
 export const signup_Post = async (req, res, next) => {
+  const { name, email, password, age, gender, genderPreference } = req.body;
   try {
+    if (!name || !email || !password || !age || !gender || !genderPreference) {
+      return next(errorHandler(400, "Please provide all fields"));
+    }
+    // !Find The User:
+    const user = await User.findOne({ email });
+    if (user) {
+      return next(errorHandler(400, "User already exists"));
+    }
+    // !Check The Age:
+    if (age < 18) {
+      return next(errorHandler(400, "Age must be greater than 18"));
+    }
+    // !Check The Password:
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if (!passwordRegex.test(password)) {
+      return next(
+        errorHandler(
+          400,
+          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        )
+      );
+    }
+    //! Create New User:
+    const newUser = new User({
+      name,
+      email,
+      password,
+      age,
+      gender,
+      genderPreference,
+    });
+    if (newUser) {
+      // !Create Token:
+      const token = createToken(newUser._id);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+      await newUser.save();
+      const { password: pass, ...rest } = newUser._doc;
+      res.status(200).json(rest);
+    } else {
+      return next(errorHandler(400, "User Already Exists"));
+    }
   } catch (error) {
     console.log("Error Creating Sign Up User", error.message);
     next(error);
